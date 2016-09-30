ad538ffdc5295956a8b779633e1e30afa9153489
Lakshmi Narasimhan <badri.dilbert@gmail.com>
2016-09-30 19:23:37 +0530

    custom access check

--- src/Controller/HelloWorldController.php
+++ src/Controller/HelloWorldController.php
@@ -3,6 +3,8 @@
 namespace Drupal\myroute\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Access\AccessResult;
 
 /**
  * Class HelloWorldController.
@@ -24,4 +26,14 @@ class HelloWorldController extends ControllerBase {
     ];
   }
 
+  /**
+   * Custom access check
+   *
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   access checking done against this account.
+   */
+  public function custom_access_check(AccountInterface $account) {
+    return AccessResult::allowedIf($account->hasPermission('access content') &&
+      $account->hasPermission('administer content'));
+  }
 }
